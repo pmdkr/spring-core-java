330c4338bd0976c714b2bf5736d71567dcbe01e9
pmdkr <pramodkrlohra@gmail.com>
2026-02-07 20:46:33 +0530

    first commit

---
+++ SpringEx1/src/in/scalive/beans/Student.java
@@ -0,0 +1,27 @@
+package in.scalive.beans;
+
+public class Student {
+	public int getRoll_no() {
+		return roll_no;
+	}
+
+	public void setRoll_no(int roll_no) {
+		this.roll_no = roll_no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Student() {
+		super();
+	}
+
+	private int roll_no;
+	private String name;
+
+}
